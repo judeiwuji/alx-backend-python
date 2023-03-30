1d3268e6ba2c040f6d2a56526ef4364b88ee8c73
judeiwuji <judeiwuji@gmail.com>
2023-03-30 12:03:46 -0700

    Add: task 7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module: Complex types - string and int/float to tuple"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Create a tuple
+
+    Args:
+        k(str)
+        v(int | float)
+    """
+    return tuple([k, v])
