efae5e77c119117aaf8885b779c1c13038bca66e
judeiwuji <judeiwuji@gmail.com>
2023-03-30 11:31:10 -0700

    Add: Task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module: Floor"""
+import math
+
+
+def floor(n: float) -> int:
+    """Calculates the floor of a number
+
+    Args:
+        n(float)
+    """
+    return math.floor(n)
