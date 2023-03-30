b475e5ec1facf328c2b38709173047096e282506
judeiwuji <judeiwuji@gmail.com>
2023-03-30 11:35:06 -0700

    Add: Task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Module: to_str"""
+
+
+def to_str(n: float) -> str:
+    """Converts float to str
+    
+    Args:
+        n(float)
+    """
+    return "{:.2f}".format(n)
