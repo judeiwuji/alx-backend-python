39630d0dc9738db59e471f90503b9a51446d1e1d
judeiwuji <judeiwuji@gmail.com>
2023-03-30 12:50:27 -0700

    Fixed errors

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 """Module: element of a sequence"""
-from typing import List, Union
+from typing import Tuple, List
 
 
-def zoom_array(lst: List[int], factor: Union[int, float] = 2) -> List[int]:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """zoom array"""
-    zoomed_in: List[int] = [
+    zoomed_in: List = [
         item for item in lst
         for i in range(int(factor))
     ]
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
-zoom_3x = zoom_array(array, 3.0)
+zoom_3x = zoom_array(array, 3)
