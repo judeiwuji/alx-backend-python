c2a1b59936007be5d7e25554028301e32d4f096d
judeiwuji <judeiwuji@gmail.com>
2023-03-30 12:13:53 -0700

    Add: Task 9

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module: iterable object"""
+from typing import Iterable, List, Tuple, Sequence
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    return [(i, len(i)) for i in lst]
