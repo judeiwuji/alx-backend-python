f09626d18ad900e42909e6d874b9bcba6ffa2800
judeiwuji <judeiwuji@gmail.com>
2023-03-30 11:25:29 -0700

    Add: Task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module: Concatenate"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings
+
+    Args:
+        str1(str)
+        str2(str)
+    """
+    return '{}{}'.format(str1, str2)
