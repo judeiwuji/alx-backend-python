2dac9fc3f1938ab80531d4e71f0129dabe94b4a5
judeiwuji <judeiwuji@gmail.com>
2023-03-30 11:20:27 -0700

    Add: Task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module: Add floats"""
+
+
+def add(a: float, b: float) -> float:
+    """Add two floats
+
+    Args:
+        a(float)
+        b(float)
+    """
+    return a + b
