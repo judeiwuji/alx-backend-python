22b0dfff297e1f26e6b8e5e6daa69d4c7a15d8ad
judeiwuji <judeiwuji@gmail.com>
2023-04-03 08:52:07 -0700

    Add: Task 4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,15 @@
+"""Module: Multiple tasks"""
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Task Wait N
+
+    Args:
+        n(int)
+        max_delay:(int)
+    """
+    results = await asyncio.gather(*(task_wait_random(max_delay) for _ in range(n)))
+    return results
