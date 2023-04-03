1a8368c28f58e41305307a78f47bcdc848ca501b
judeiwuji <judeiwuji@gmail.com>
2023-04-03 07:41:22 -0700

    update

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 """Module: Basic async"""
-import asyncio
 import random
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """Wait random
 
     Args:
         max_delay(int)
     """
-    await asyncio.sleep(max_delay)
     return random.uniform(0, max_delay)
