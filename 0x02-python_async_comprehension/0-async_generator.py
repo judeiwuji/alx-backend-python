74e96eaec89c5289c4157baa9324d487e002d98f
judeiwuji <judeiwuji@gmail.com>
2023-04-04 06:45:10 -0700

    update

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """Module: """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Async Generator: generates numbers
     """
     for i in range(10):
