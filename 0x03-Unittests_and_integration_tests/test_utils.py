753304ab597f30e0e4fafd6c5c0181e8de68284d
judeiwuji <judeiwuji@gmail.com>
2023-04-23 18:18:42 +0100

    updates

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -16,7 +16,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ("nested_map", {'nested_map': {
          "a": {"b": 2}}, 'path': ("a", "b"), }, 2)
     ])
-    def test_access_nested_map(self, _: str, input: Dict,
+    def test_access_nested_map(self, name: str, input: Dict,
                                expected: Union[int, Dict]):
         """Test cases for test_access_nested_map"""
         self.assertEqual(utils.access_nested_map(**input), expected)
