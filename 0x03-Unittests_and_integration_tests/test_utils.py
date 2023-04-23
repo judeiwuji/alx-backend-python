282043d071b55923f8f994c9dc42f1364c1ff1c0
judeiwuji <judeiwuji@gmail.com>
2023-04-23 18:36:23 +0100

    update

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -28,7 +28,8 @@ class TestAccessNestedMap(unittest.TestCase):
          'path': ("a", "b"), }, KeyError)
     ])
     def test_access_nested_map_exception(self, name: str,
-                                         input: Dict[str, Union[Dict, Tuple[str]]],
+                                         input:
+                                         Dict[str, Union[Dict, Tuple[str]]],
                                          expected: Union[int, Dict]):
         """Test access nested map exception"""
         with self.assertRaises(expected):
