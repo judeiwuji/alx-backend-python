c454e8da19459a849e693cffb0551a780812a36e
judeiwuji <judeiwuji@gmail.com>
2023-04-24 07:08:56 +0100

    Add: Task 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,6 +5,7 @@ from unittest import mock
 from parameterized import parameterized
 from typing import Dict, Union, Tuple
 utils = __import__("utils")
+memoize = utils.memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -58,3 +59,24 @@ class TestGetJson(unittest.TestCase):
         self.assertEqual(utils.get_json(
             "http://holberton.io"), mock_response.json())
         mock_requests.get.assert_called_with("http://holberton.io")
+
+
+class TestMemoize(unittest.TestCase):
+    """Test Memoize"""
+
+    def test_memoize(self):
+        """Test memoize"""
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with mock.patch.object(TestClass, 'a_method', return_value=42) as mock_method:
+            test = TestClass()
+            test.a_property
+            test.a_property
+        mock_method.assert_called_once()
