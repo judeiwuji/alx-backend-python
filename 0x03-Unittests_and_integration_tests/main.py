e4bb0d8805200e57d2d555ef2149fa8acde7a9d9
judeiwuji <judeiwuji@gmail.com>
2023-04-25 15:13:19 +0100

    update

--- 0x03-Unittests_and_integration_tests/main.py
+++ 0x03-Unittests_and_integration_tests/main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 from client import (GithubOrgClient)
 utils = __import__("utils")
-# print(utils.access_nested_map({"a": 1}, path=("a",)))
-# print(utils.access_nested_map({"a": {"b": 2}}, path=("a",)))
-# print(utils.access_nested_map({"a": {"b": 2}}, path=("a", "b")))
-client = GithubOrgClient("google")
-print(GithubOrgClient.has_license(
-    repo={"license": {"key": "my_license"}}, license_key="my_license"))
+print(utils.access_nested_map({"a": 1}, path=("a",)))
+print(utils.access_nested_map({"a": {"b": 2}}, path=("a",)))
+print(utils.access_nested_map({"a": {"b": 2}}, path=("a", "b")))
+# client = GithubOrgClient("google")
+# print(GithubOrgClient.has_license(
+#     repo={"license": {"key": "my_license"}}, license_key="my_license"))
--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -12,17 +12,16 @@ class TestAccessNestedMap(unittest.TestCase):
     """Test cases for test_access_nested_map"""
 
     @parameterized.expand([
-        ("nested_map", {'nested_map': {"a": 1}, 'path': ("a",), }, 1),
-        ("nested_map", {'nested_map': {
-         "a": {"b": 2}}, 'path': ("a",), }, {'b': 2}),
-        ("nested_map", {'nested_map': {
-         "a": {"b": 2}}, 'path': ("a", "b"), }, 2)
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, name: str,
-                               input: Dict[str, Union[Dict, Tuple]],
+    def test_access_nested_map(self, nested_map: Dict,
+                               path: Tuple,
                                expected: Union[int, Dict]):
         """Test cases for test_access_nested_map"""
-        self.assertEqual(utils.access_nested_map(**input), expected)
+        self.assertEqual(utils.access_nested_map(
+            nested_map, path), expected)
 
     @parameterized.expand([
         ({}, ("a",)),
