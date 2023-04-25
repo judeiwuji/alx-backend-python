b4c1cd5adbb087500f990f1a5e10a6dc0af22838
judeiwuji <judeiwuji@gmail.com>
2023-04-25 14:38:22 +0100

    update

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Module: TestGithubOrgClient"""
 import unittest
 from unittest.mock import patch, MagicMock, Mock, PropertyMock
 from parameterized import parameterized, parameterized_class
